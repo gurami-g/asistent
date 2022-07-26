ba0be35e35adddaffadaf3a6ad8cea7d28dbfb30
gurami-g <72502288+gurami-g@users.noreply.github.com>
2022-07-27 02:24:32 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+Listener = sr.Recognizer()
+engine = pyttsx3.init()
+
+rate = engine.getProperty('rate')
+engine.setProperty('rate', 120)
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+
+def talk(text):
+    engine.say('text')
+    engine.runAndWait()
+
+def take_commamd():
+    try:
+        with sr.Microphone() as source:
+            print('listening... ')
+            voice = Listener.listen(source)
+            command = Listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:   
+                command = command.replace('alexa', '')     
+                talk(command)
+    except:
+        pass
+    return command
+
+def run_alexa():
+    command = take_commamd()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('current time is' + time)
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '0')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I habe an headache')
+    elif 'are you single' in command:
+        talk('I am in relationship with wifi')
+    
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+
+    else:
+        talk('please say the command again.')
+
+
+while True:
+    run_alexa()
